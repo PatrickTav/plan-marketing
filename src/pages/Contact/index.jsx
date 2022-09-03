dedec623e6e0fed0b43f7aa1ceccf4fb3f8a9221
Patrick Tavares <patricktavares3pc@gmail.com>
2022-09-03 13:34:24 -0300

    main: inserindo o form na page contact

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -3,33 +3,37 @@ import Arrow from "../../assets/Arrow.png"
 import { Container_section } from './style'
 
 const Contact = () => {
+
+  const handleSubmit =(e) =>{
+    e.preventDefault()
+  }
+
+
+
+
   return (
     <Container_section>
       <div className="form-container">
         <span className="back">05.</span>
         <h2>/Contanto</h2>
-        <p>
-          Gostou de saber mais sobre as inovações tecnológicas e também quer
-          oferecer novas experiências aos seus clientes? Então entre em contato
-          e marque um encontro real conosco para conversamos sobre as infinitas
-          possibilidades do mundo virtual.
-          
-        </p>
-        <form >
+        
+        <form onSubmit={handleSubmit} >
           <label>
-            Nome:
+            Nome: <br />
             <input type="text" />
           </label>
+          <div>
+            <label>
+              E-mail
+              <input type="email" />
+            </label>
+            <label>
+              Telefone:
+              <input type="tel" />
+            </label>
+          </div>
           <label>
-            E-mail:
-            <input type="email" />
-          </label>
-          <label>
-            Telefone:
-            <input type="tel" />
-          </label>
-          <label>
-            Mensagem:
+            Mensagem:<br />
             <textarea name="" id="" cols="30" rows="10"></textarea>
           </label>
           <button>Enviar <img src={Arrow} alt="" /></button>
--- src/pages/Contact/style.js
+++ src/pages/Contact/style.js
@@ -1,9 +1,65 @@
 import styled from 'styled-components'
 
 export const Container_section = styled.div`
+  margin-top: 200px;
   display: flex;
+  position: relative;
+  flex-wrap: nowrap;
+  gap: 5px;
   & {
     .form-container {
+      display: flex;
+      flex-direction: column;
+
+      h2 {
+        font-weight: 400;
+        color: ${(props) => props.theme.colors.fontSecondary};
+      }
+
+      .back {
+        position: absolute;
+        font-weight: 700;
+        font-size: 5.2rem;
+        color: #272727;
+        top: -40px;
+        z-index: -1;
+      }
+
+      form {
+        display: flex;
+        flex-direction: column;
+        margin-top: 90px;
+        gap: 10px;
+      }
+
+      form input,
+      textarea {
+        background-color: transparent;
+        width: 100%;
+        border: 2px solid ${(props) => props.theme.colors.fontSecondary};
+        border-radius: 0 15px 0 0;
+      }
+      label{
+        font-size: .9rem;
+      }
+      form input {
+        
+        padding: 0.4rem;
+      }
+      form textarea {
+        resize: none;
+      }
+
+      form button {
+        background-color: transparent;
+        border: none;
+        text-align: start;
+        cursor: pointer;
+        transition: all .4s;
+      }
+      form button:hover {
+        color: ${(props) => props.theme.colors.hover};
+      }
     }
 
     .partnership_adress {
